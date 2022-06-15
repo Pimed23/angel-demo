8260d41a78784b93512a5520c3a43a2499174fca
pimed23 <b.pimed@gmail.com>
2022-06-15 04:30:05 -0300

    Smart contract

--- compile.js
+++ compile.js
@@ -4,7 +4,6 @@ const solc = require('solc');
 
 const contractPath = path.resolve(__dirname, 'src/contracts', 'Angel.sol');
 const source = fs.readFileSync(contractPath, 'utf-8');
-
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
-
 //console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Monitor'];
+
--- src/contracts/Angel.sol
+++ src/contracts/Angel.sol
@@ -1,13 +1,39 @@
 pragma solidity ^0.4.17;
 
-contract Inbox {
-    string public message;
-    
-    function Inbox(string initialMessage) public {
-        message = initialMessage;
-    }
-    
-    function setMessage(string newMessage) public {
-        message = newMessage;
-    }
+contract Monitor {
+  struct Measure {
+    uint start_timestamp;
+    uint end_timestamp;
+    address equipment_address;
+    address monitored_person;
+    bool is_normal;
+  }
+  
+  Measure[] public measures;
+  mapping(address => address) public equipment_to_person;
+  address public manager;
+  address public patient;
+
+  function Monitor( address creator, address target ) public {
+      manager = creator;
+      patient = target;
+  }
+
+  function addEquipment(address equipment, address person) public {
+      equipment_to_person[equipment] = person;
+  }
+  
+  function createMeasure(uint start, uint end, address equipment, address target, bool is_normal ) public {
+    require(end-start > 50);
+    require(equipment_to_person[equipment] == target);
+
+    Measure memory newMeasure = Measure({
+      start_timestamp : start,
+      end_timestamp : end,
+      equipment_address : equipment,
+      monitored_person : target,
+      is_normal : is_normal
+    });
+    measures.push( newMeasure );
+  }
 }
