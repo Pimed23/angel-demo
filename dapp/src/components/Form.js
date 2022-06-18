668cc8c6efb76bd0044aa68be11f895555d65439
pimed23 <b.pimed@gmail.com>
2022-06-18 16:32:11 -0300

    Disconnect button

--- dapp/src/components/Form.js
+++ dapp/src/components/Form.js
@@ -5,25 +5,41 @@ import monitor from '../monitor.js';
 import web3 from '../web3.js';
 
 const Form = () => {
+    const [accountAddress, setAccountAddress] = useState('');
     const [manager, setManager] = useState('');
     const [patient, setPatient] = useState('');
     const [device, setDevice] = useState('');
     const [deviceAddress, setDeviceAddress] = useState('');
 
     useEffect(() => {
-        const getManager = async () => {
-            const manager = await monitor.methods.manager().call();
-            setManager(manager);    
+        const checkingCredentials = async () => {
+            getPatient().then((patient) => {
+                if (patient === accountAddress) {
+                    getManager();
+                    setPatient(patient);
+                    getDevice(patient);
+                } 
+            });
         }
-        getManager();
+        checkingCredentials();
+    }, [accountAddress]);
 
-        const getPatient = async () => {
-            const patient = await monitor.methods.patient().call();
-            setPatient(patient);
-            getDevice(patient);
-        }
-        getPatient();    
-    }, []);
+    const getAccount = async () => {
+        await window.ethereum.request({method: 'eth_requestAccounts'});
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        return account;
+    }
+    
+    const getPatient = async () => {
+        const patient = await monitor.methods.patient().call();
+        return patient;
+    }
+
+    const getManager = async () => {
+        const manager = await monitor.methods.manager().call();
+        setManager(manager);
+    }
 
     const getDevice = async (patient) => {
         try {
@@ -34,17 +50,37 @@ const Form = () => {
         }
     }
 
-    const connectIoTDevice = async(event, params) => {
-        event.preventDefault();
+    const connectIoTDevice = async (event, params) => {
         const accounts = await web3.eth.getAccounts();
         await monitor.methods.addEquipment(params[0], params[1]).send({
             from: accounts[0]
         });
     }
+
+    const connectMetamask = async (event) => {
+        event.preventDefault();
+        if (typeof window.ethereum !== 'undefined') {
+            getAccount().then((account) => {
+                setAccountAddress(account)
+            });
+
+        } else {
+            alert("Metamask is not installed");
+        }
+    }
+
+    const disconnectMetamask = async (event) => {
+        window.ethereum.on('disconnect', setAccountAddress('0x0'));
+    }
     
     return (
         <form>
             <div className = 'row'>
+                <ActionButton
+                    title = {!!accountAddress ? 'Disconnect' : 'Connect to Metamask'}
+                    action = {!!accountAddress ?    disconnectMetamask : connectMetamask}
+                />
+
                 <DataLabel
                     title = 'Medic center ID'
                     data = {!!manager ? manager : '---'}
