5533df7866fa77b6e0d1c11be3abf9825f688717
pimed23 <b.pimed@gmail.com>
2022-06-17 22:59:24 -0300

    Setting contract

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import Form from './components/Form';
+import Form from './components/Form.js';
+import web3 from './web3.js';
+import monitor from './monitor.js';
 
 function App() {
+  console.log(web3.version);
   return (
       <div className='container'>
         <Form/>
